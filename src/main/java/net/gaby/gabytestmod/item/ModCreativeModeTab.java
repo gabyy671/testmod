8d93965bd575b65bdedca1bdca462eee204b3ad4
gabyy671 <gabylivelol123@gmail.com>
2023-12-29 19:14:12 +0100

    added recipies and a ressource pack to then test env

--- src/main/java/net/gaby/gabytestmod/item/ModCreativeModeTab.java
+++ src/main/java/net/gaby/gabytestmod/item/ModCreativeModeTab.java
@@ -1,5 +1,6 @@
 package net.gaby.gabytestmod.item;
 
+import net.gaby.gabytestmod.block.ModBlocks;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -8,7 +9,7 @@ public class ModCreativeModeTab {
     public static final CreativeModeTab TEST_TAB = new CreativeModeTab("testtab") {
         @Override
         public ItemStack makeIcon() {
-            return new ItemStack(ModItems.RAW_BITTERGEM.get());
+            return new ItemStack(ModBlocks.BITTERGEM_ORE.get());
         }
     };
 }
