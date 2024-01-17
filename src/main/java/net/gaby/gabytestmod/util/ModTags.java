4bcdb995f8611a8c46e33bde1a7592ac27d7478a
gabyy671 <gabylivelol123@gmail.com>
2024-01-17 16:12:13 +0100

    started the tags

---
+++ src/main/java/net/gaby/gabytestmod/util/ModTags.java
@@ -0,0 +1,16 @@
+package net.gaby.gabytestmod.util;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+
+public class ModTags {
+    public static class Blocks {
+        public static final Tags.IOp
+
+    }
+
+
+    public static class Items {
+
+    }
+}
